6b90a499201050c05fe63a0d9a8397e87244c57c
Sara Kane <kane.sarak@gmail.com>
2020-06-30 12:23:18 -0700

    Add passing test for 'Should be able to get one question'
    Co-authored-by: Noel Kirkland <noelkirkland@gmail.com>

--- __tests__/flash-cards.test.js
+++ __tests__/flash-cards.test.js
@@ -5,34 +5,42 @@ describe('FlashCard', () => {
 
   beforeEach(() => {
     reusableFlashCard = new FlashCard();
-  })
-  test('Should create a new FlashCard object', () => {
-    expect(reusableFlashCard).toMatchObject({card1: {
-      question: "What variable type is identified with squar brackets?",
-      answer: "an array",
-      fakeAnswers: ["an object", "a string", "a booleon"]
-    },
-    card2: {
-      question: "How many values can an object contain?",
-      answer: "infinite",
-      fakeAnswers: ["100", "25,000", "1,000,000"]
-    },
-    card3: {
-      question: "How many values can an object contain?",
-      answer: "4,294,967,295",
-      fakeAnswers: ["100", "25,000", "1,000,000"]
-    },
-    card4: {
-      question: "What is Sara's color?",
-      answer: "purple",
-      fakeAnswers: ["red", "green", "blue"]
-    },
-    card5: {
-      question: "When was Javascript invented in it's very first itteration?",
-      answer: "1995",
-      fakeAnswers: ["1975", "1985", "1990"]
-    }
-  });
   });
 
+  test('Should create a new FlashCard object', () => {
+    expect(reusableFlashCard.questions).toEqual([{
+      question: "What variable type is identified with square brackets?",
+      answer: "an array",
+      fakeAnswers: ["an object", "a string", "a booleon"]
+      },
+      {
+        question: "How many values can an object contain?",
+        answer: "infinite",
+        fakeAnswers: ["100", "25,000", "1,000,000"]
+      },
+      {
+        question: "How many values can an object contain?",
+        answer: "4,294,967,295",
+        fakeAnswers: ["100", "25,000", "1,000,000"]
+      },
+      {
+        question: "What is Sara's color?",
+        answer: "purple",
+        fakeAnswers: ["red", "green", "blue"]
+      },
+      {
+        question: "When was Javascript invented in it's very first itteration?",
+        answer: "1995",
+        fakeAnswers: ["1975", "1985", "1990"]
+      }
+    ]);
+  });
+
+  test('Should be able to get one question', () => {
+    expect(reusableFlashCard.getCard(4)).toMatchObject({   
+      question: "When was Javascript invented in it's very first itteration?",
+      answers: ["1975", "1985", "1990", "1995"],
+      correctAnswer: "1995"
+    });
+  });
 });
--- src/flash-cards.js
+++ src/flash-cards.js
@@ -1,29 +1,59 @@
 export class FlashCard {
   constructor() {
-    this.card1 = {
-      question: "What variable type is identified with squar brackets?",
-      answer: "an array",
-      fakeAnswers: ["an object", "a string", "a booleon"]
-    };
-    this.card2 = {
-      question: "How many values can an object contain?",
-      answer: "infinite",
-      fakeAnswers: ["100", "25,000", "1,000,000"]
-    };
-    this.card3 = {
-      question: "How many values can an object contain?",
-      answer: "4,294,967,295",
-      fakeAnswers: ["100", "25,000", "1,000,000"]
-    };
-    this.card4 = {
-      question: "What is Sara's color?",
-      answer: "purple",
-      fakeAnswers: ["red", "green", "blue"]
-    };
-    this.card5 = {
-      question: "When was Javascript invented in it's very first itteration?",
-      answer: "1995",
-      fakeAnswers: ["1975", "1985", "1990"]
-    };
+    this.questions = [
+        {
+        question: "What variable type is identified with square brackets?",
+        answer: "an array",
+        fakeAnswers: ["an object", "a string", "a booleon"]
+      },
+      {
+        question: "How many values can an object contain?",
+        answer: "infinite",
+        fakeAnswers: ["100", "25,000", "1,000,000"]
+      },
+      {
+        question: "How many values can an object contain?",
+        answer: "4,294,967,295",
+        fakeAnswers: ["100", "25,000", "1,000,000"]
+      },
+      {
+        question: "What is Sara's color?",
+        answer: "purple",
+        fakeAnswers: ["red", "green", "blue"]
+      },
+      {
+        question: "When was Javascript invented in it's very first itteration?",
+        answer: "1995",
+        fakeAnswers: ["1975", "1985", "1990"]
+      }
+    ];
   }
+
+  getCard(cardNumber){
+    let returnObj = {};
+    returnObj.question = this.questions[cardNumber].question;
+    returnObj.answers = this.questions[cardNumber].fakeAnswers;
+    returnObj.answers[returnObj.answers.length] = this.questions[cardNumber].answer;
+    returnObj.answers.sort();
+    returnObj.correctAnswer = this.questions[cardNumber].answer;
+    return returnObj;
+  }
+
+
+  /* 
+    displayQuestions(){
+      setInterval(() => {
+        $('#span-id').show(this.card1.question)
+      }20000);
+    if (this.card1.answer === our user's click){
+      clearInterval()
+      User.score ++,
+    } else (this.card1.answer !== our user's click){
+        setInterval(() => {
+        $('#span-id').show(this.card2.question)
+      }20000);
+    }
+  }
+  */
+  
 }
